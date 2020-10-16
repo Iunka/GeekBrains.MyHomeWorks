6a7400b75bac8a3e7b2beced786bb2aeaa711a68
StaSia <kirstasia@gmail.com>
2020-10-16 23:13:49 +0300

    task4 done

---
+++ src/Task4.java
@@ -0,0 +1,9 @@
+public class Task4 {
+    public static void main(String [] args){
+        System.out.println(Hello("Андрей!"));
+    }
+    public static String Hello(String name){
+        String a = "Привет, " + name;
+        return a;
+    }
+}
